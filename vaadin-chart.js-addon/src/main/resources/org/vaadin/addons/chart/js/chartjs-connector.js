95e18df9411a00a6f706a624a5a338881b8acf7c
felixhusse <felix.husse@gmail.com>
2014-11-05 11:00:43 +0100

    -initial commit

---
+++ vaadin-chart.js-addon/src/main/resources/org/vaadin/addons/chart/js/chartjs-connector.js
@@ -0,0 +1,88 @@
+/* 
+ * Copyright 2014 fatalix.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+window.org_vaadin_addons_chart_js_ChartJS = function () {
+    var e = this.getElement();
+
+    this.onStateChange = function () {
+        initChartJS('myChart');
+    };
+
+
+
+    initChartJS = function (id) {
+        e.innerHTML = e.innerHTML + "<canvas id='"+id+"' width='400' height='400'></canvas>";
+        
+        var data = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    //fillColor: "rgba(220,220,220,0.2)",
+                    //strokeColor: "rgba(220,220,220,1)",
+                    //pointColor: "rgba(220,220,220,1)",
+                    //pointStrokeColor: "#fff",
+                    //pointHighlightFill: "#fff",
+                    //pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: "My Second dataset",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+        
+        var options = {
+            ///Boolean - Whether grid lines are shown across the chart
+            scaleShowGridLines : true,
+            //String - Colour of the grid lines
+            scaleGridLineColor : "rgba(0,0,0,.05)",
+            //Number - Width of the grid lines
+            scaleGridLineWidth : 1,
+            //Boolean - Whether the line is curved between points
+            bezierCurve : false,
+            //Number - Tension of the bezier curve between points
+            //bezierCurveTension : 0.4,
+            //Boolean - Whether to show a dot for each point
+            pointDot : true,
+            //Number - Radius of each point dot in pixels
+            pointDotRadius : 4,
+            //Number - Pixel width of point dot stroke
+            pointDotStrokeWidth : 1,
+            //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+            pointHitDetectionRadius : 20,
+            //Boolean - Whether to show a stroke for datasets
+            datasetStroke : true,
+            //Number - Pixel width of dataset stroke
+            datasetStrokeWidth : 2,
+            //Boolean - Whether to fill the dataset with a colour
+            //datasetFill : true,
+            //String - A legend template
+            //legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+        }; 
+        var ctx = document.getElementById(id).getContext("2d");
+        var myNewChart = new Chart(ctx).Line(data,options);
+
+    };
+
+};
+
---
+++ vaadin-chart.js-demo/src/main/java/de/fatalix/vaadin/addon/codemirror/demo/DemoUI.java
@@ -0,0 +1,56 @@
+package de.fatalix.vaadin.addon.codemirror.demo;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import org.vaadin.addons.chart.js.ChartJS;
+
+@Theme("demo")
+@Title("ChartJS Add-on Demo")
+@SuppressWarnings("serial")
+public class DemoUI extends UI {
+
+    private static final String exampleCode = "function findSequence(goal) {\n" + "  function find(start, history) {\n" + "    if (start == goal)\n" + "      return history;\n"
+                                              + "    else if (start > goal)\n" + "      return null;\n" + "    else\n" + "      return find(start + 5, \"(\" + history + \" + 5)\") ||\n"
+                                              + "             find(start * 3, \"(\" + history + \" * 3)\");\n" + "  }\n" + "  return find(1, \"1\");\n" + "}";
+
+    private static final String exampleCode2 = "function findDaSequenceeee(goal) {\n" + "  function doSomeSearching(start, history) {\n" + "  }\n" + "  return find(1, \"1\");\n" + "}";
+
+    private boolean changeFlag = false;
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(final VaadinRequest request) {
+        try {
+
+            // Show it in the middle of the screen
+            final VerticalLayout layout = new VerticalLayout();
+            layout.setStyleName("demoContentLayout");
+            final ChartJS chartJS = new ChartJS();
+            chartJS.setHeight(500, Unit.PIXELS);
+            chartJS.setWidth(600, Unit.PIXELS);
+
+
+            layout.addComponents(chartJS);
+            layout.setExpandRatio(chartJS, 1.0f);
+            layout.setComponentAlignment(chartJS, Alignment.MIDDLE_CENTER);
+            layout.setSizeFull();
+
+            setContent(layout);
+
+        } catch(Exception ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
+}
